d441ba2973529918b646981d73785793d8ab521e
John Doe <johndoe@example.com>
2020-09-04 02:25:54 +0400

    add advantages-windows.html and advantages-windows.scss files

--- wp-content/themes/twentytwenty-child/js/main.js
+++ wp-content/themes/twentytwenty-child/js/main.js
@@ -1,2 +1,15 @@
-// В основном будут скрипты вызова слайдеров
+$(document).ready(function(){
+
+    $(".advantages-windows__slider").owlCarousel({
+        items: 1,
+        nav: true,
+        navText: [''],
+        navContainerClass: ['advantages-windows__nav'],
+        navClass: ['advantages-windows__nav-buttons advantages-windows__nav-buttons--left', 
+                    'advantages-windows__nav-buttons advantages-windows__nav-buttons--right'],
+        dotsClass: ['advantages-windows__nav-dots'],
+        dotClass: ['advantages-windows__nav-dot']
+    });
+    
+});
 
