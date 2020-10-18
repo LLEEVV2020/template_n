c27d5b205bb40353a4f3877202e1f89e9492018a
LLEEVV2020 <lleevv2020@gmail.com>
2020-10-18 18:28:54 +0400

    add

--- wp-content/themes/twentytwenty-child/js/calc_b.js
+++ wp-content/themes/twentytwenty-child/js/calc_b.js
@@ -1,2 +1,42 @@
 'use strict';
 
+// счётчик, на каком шаге находится текущий шаг
+let currentSlide = 0;
+
+// навигация
+const previousButton = document.getElementById("previous");
+const nextButton = document.getElementById("next");
+const stepWrapper = document.getElementById("step-wrapper");
+
+previousButton.addEventListener("click", showPreviousSlide);
+nextButton.addEventListener("click", showNextSlide);
+
+// перелистыватель вперёд ( кнопка "вперёд")
+function showNextSlide() {
+    showSlide(currentSlide + 1);
+}
+// перелистыватель назад ( кнопка "назад")
+function showPreviousSlide() {
+    showSlide(currentSlide - 1);
+}
+
+// ф-ция в которой происходит перелистывание шагов
+function showSlide(n) {
+
+    stepWrapper.classList.remove("balcony-home", 
+    "balcony-leaflets", "balcony-size", "balcony-add",
+    "balcony-services", "balcony-payment");
+
+    if(currentSlide === 0){
+        stepWrapper.classList.add("balcony-home");
+        previousButton.classList.add("none");
+    }
+    if(currentSlide === 1){
+        stepWrapper.classList.add("balcony-leaflets");
+        previousButton.classList.remove("none");
+    }
+
+}
+
+// Вызываем первый экран
+showSlide(currentSlide);
