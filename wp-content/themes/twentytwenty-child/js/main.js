9faf4615d06ce1f7c7a2545033fabdc4cbc1c284
jamesborn89 <jamesborn89@gmail.com>
2020-12-17 12:29:04 +0400

    dfix btn

--- wp-content/themes/twentytwenty-child/js/main.js
+++ wp-content/themes/twentytwenty-child/js/main.js
@@ -925,7 +925,7 @@ $(document).ready(function () {
     if (windowWidth < 1200) {
 
       elem.addClass('owl-carousel');
-      
+
       $('.installment-plan__slider-new').owlCarousel({
         items: 1,
         dots: true,
@@ -950,7 +950,7 @@ $(document).ready(function () {
   $(window).resize(function () {
     checkWidth11(); // проверит при изменении размера окна клиента
   });
-  
+
   $('.installing-windows-in-5-days__slider').owlCarousel({
     items: 1,
     dots: true,
@@ -960,19 +960,94 @@ $(document).ready(function () {
     margin: 16,
     stagePadding: 60,
     responsive: {
-        768: {
-            items: 3,
-            stagePadding: false,
-            margin: 15
-        },
-        1200: {
-          items: 4,
-          stagePadding: false,
-          nav: true,
-          navContainerClass: ['installing-windows-in-5-days__navigation'],
-          navClass: ['installing-windows-in-5-days__navigation-left', 'installing-windows-in-5-days__navigation-right'],
-        }
+      768: {
+        items: 3,
+        stagePadding: false,
+        margin: 15
+      },
+      1200: {
+        items: 4,
+        stagePadding: false,
+        nav: true,
+        navContainerClass: ['installing-windows-in-5-days__navigation'],
+        navClass: ['installing-windows-in-5-days__navigation-left', 'installing-windows-in-5-days__navigation-right'],
+      }
     }
-});
+  });
+
+
+    // слайдер для блока company-reviews
+
+  var sync1 = $("#sync1");
+  var sync2 = $("#sync2");
+  var slidesPerPage = 4; //globaly define number of elements per page
+  var syncedSecondary = true;
+
+  sync1.owlCarousel({
+    items: 1,
+    nav: true,
+    loop: true,
+    dots: false,
+  }).on('changed.owl.carousel', syncPosition);
+
+  sync2
+    .on('initialized.owl.carousel', function () {
+      sync2.find(".owl-item").eq(0).addClass("current");
+    })
+    .owlCarousel({
+      items : slidesPerPage,
+      autoWidth: true,
+      dots: false,
+      slideBy: slidesPerPage, //alternatively you can slide by 1, this way the active slide will stick to the first item in the second carousel
+      responsiveRefreshRate: 100
+    }).on('changed.owl.carousel', syncPosition2);
+
+  function syncPosition(el) {
+    //if you set loop to false, you have to restore this next line
+    //var current = el.item.index;
+
+    //if you disable loop you have to comment this block
+    var count = el.item.count - 1;
+    var current = Math.round(el.item.index - (el.item.count / 2) - .5);
+
+    if (current < 0) {
+      current = count;
+    }
+    if (current > count) {
+      current = 0;
+    }
+
+    //end block
+
+    sync2
+      .find(".owl-item")
+      .removeClass("current")
+      .eq(current)
+      .addClass("current");
+    var onscreen = sync2.find('.owl-item.active').length - 1;
+    var start = sync2.find('.owl-item.active').first().index();
+    var end = sync2.find('.owl-item.active').last().index();
+
+    if (current > end) {
+      sync2.data('owl.carousel').to(current, 100, true);
+    }
+    if (current < start) {
+      sync2.data('owl.carousel').to(current - onscreen, 100, true);
+    }
+  }
+
+  function syncPosition2(el) {
+    if (syncedSecondary) {
+      var number = el.item.index;
+      sync1.data('owl.carousel').to(number, 100, true);
+    }
+  }
+
+  sync2.on("click", ".owl-item", function (e) {
+    e.preventDefault();
+    var number = $(this).index();
+    sync1.data('owl.carousel').to(number, 300, true);
+  });
+
 
 });
